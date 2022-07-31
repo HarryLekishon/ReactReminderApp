7e0587677e03e7bbda6dacd185d5f1e92cbc71b1
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-07-31 13:49:06 +0300

    final commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title, onAdd, showTask}) => {
+const Header = ({title, onAdd, showAdd}) => {
 
   return (
     <div>
       <header className='header'>
         <h1>{title}</h1>
-        <Button color={showTask ? 'red' : 'green'} text={showTask ? 'Close' : 'Add'} onClick={onAdd}/>
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
       </header>
     </div>
   )
 }
 Header.defaultProps = {
-    title: 'Task Trackers',
+    title: 'Set Reminders',
 }
 
 Header.propTypes = {
